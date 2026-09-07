12342478cd7c1f490640d96208f93ea77e042a2a
Ntlahla5408 <s226083470@Mandela.ac.za>
2026-09-07 16:19:53 +0200

    assembly commit

--- GrabnBite/GrabnBite/obj/Debug/net8.0/GrabnBite.AssemblyInfo.cs
+++ GrabnBite/GrabnBite/obj/Debug/net8.0/GrabnBite.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GrabnBite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ef35b9a9f02af2922e91c7b24ab91a80c0b9765")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4f2b0f5015fda23d28c6b4546901d9fb76e3868")]
 [assembly: System.Reflection.AssemblyProductAttribute("GrabnBite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GrabnBite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

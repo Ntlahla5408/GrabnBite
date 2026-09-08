e60e3430e84e6596f40c8b08eb380968a6067439
Sibucode <stachmaro@gmail.com>
2026-09-08 07:58:08 +0200

    Entity: updates

--- GrabnBite/GrabnBite/Models/Entities/MenuItem.cs
+++ GrabnBite/GrabnBite/Models/Entities/MenuItem.cs
@@ -3,25 +3,16 @@
     public class MenuItem
     {
         public int MenuItemId { get; set; }
-
         public string Name { get; set; } = string.Empty;
-
         public string Description { get; set; } = string.Empty;
-
         public decimal Price { get; set; }
-
         public bool IsAvailable { get; set; }
-
         // Foreign Keys
         public int RestaurantId { get; set; }
-
         public int MenuCategoryId { get; set; }
-
         // Relationships
         public Restaurant Restaurant { get; set; } = null!;
-
         public MenuCategory MenuCategory { get; set; } = null!;
-
         // An item can appear in many order items
         public ICollection<OrderItem> OrderItems { get; set; }
             = new List<OrderItem>();

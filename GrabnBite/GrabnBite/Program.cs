fe3ed650b4f94d787788ffa7a084a8421cdf37f7
Ntlahla5408 <s226083470@Mandela.ac.za>
2026-09-10 12:34:09 +0200

    changes

--- GrabnBite/GrabnBite/Program.cs
+++ GrabnBite/GrabnBite/Program.cs
@@ -19,7 +19,6 @@ builder.Services.AddCors(options =>
 });
 // Add services to the container.
 
-
 builder.Services.AddControllers();
 builder.Services.AddSignalR();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
--- GrabnBite/GrabnBite/obj/Debug/net8.0/GrabnBite.AssemblyInfo.cs
+++ GrabnBite/GrabnBite/obj/Debug/net8.0/GrabnBite.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GrabnBite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dc761cbf9649a6e183783f25e474dbfa02c3995")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+904f6db54ad1c52690434c4dc6866c9b36e3009e")]
 [assembly: System.Reflection.AssemblyProductAttribute("GrabnBite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GrabnBite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

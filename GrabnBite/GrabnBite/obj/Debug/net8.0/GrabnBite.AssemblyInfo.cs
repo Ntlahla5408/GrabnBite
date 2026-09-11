bb46bcfebda093e64e347ed25c7126a613ab76b4
orange855 <primenaturesolutions@gmail.com>
2026-09-12 01:59:06 +0200

    assembly

--- GrabnBite/GrabnBite/obj/Debug/net8.0/GrabnBite.AssemblyInfo.cs
+++ GrabnBite/GrabnBite/obj/Debug/net8.0/GrabnBite.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GrabnBite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b9a4c65df45ae2d534b99a268f9901f83330772")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7735b89d54082c61f9a65210d98cbbf1a3755627")]
 [assembly: System.Reflection.AssemblyProductAttribute("GrabnBite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GrabnBite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

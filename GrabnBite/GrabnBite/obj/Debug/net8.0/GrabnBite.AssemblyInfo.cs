74541dace32cf9950dfb40c0dda4252df75d6ba8
Sibucode <stachmaro@gmail.com>
2026-09-12 08:44:36 +0200

    feat: Add migration for restaurant update with initial structure

--- GrabnBite/GrabnBite/obj/Debug/net8.0/GrabnBite.AssemblyInfo.cs
+++ GrabnBite/GrabnBite/obj/Debug/net8.0/GrabnBite.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GrabnBite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f463b90fad6e3df16c4181b774c1c9e480bf34c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f876771a2be3ea817a9e35fc0c14a404fe9578e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("GrabnBite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GrabnBite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

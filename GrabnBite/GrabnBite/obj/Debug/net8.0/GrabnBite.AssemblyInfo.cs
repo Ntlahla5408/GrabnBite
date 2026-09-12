d581beaed09c48b5163afed40bdf1c7d5f077912
Thapelo <thapelo.thapi79@gmail.com>
2026-09-12 08:45:05 +0200

    Update AssemblyInformationalVersion in AssemblyInfo.cs
    Updated the `AssemblyInformationalVersionAttribute` from
    "1.0.0+74ec3fdc9324ba8596c09eba987e59175f89fd97"
    to "1.0.0+894a943228918c6b775a745b4ea6085fe9799889"
    to reflect a new build or versioning scheme for the application.

--- GrabnBite/GrabnBite/obj/Debug/net8.0/GrabnBite.AssemblyInfo.cs
+++ GrabnBite/GrabnBite/obj/Debug/net8.0/GrabnBite.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GrabnBite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74ec3fdc9324ba8596c09eba987e59175f89fd97")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+894a943228918c6b775a745b4ea6085fe9799889")]
 [assembly: System.Reflection.AssemblyProductAttribute("GrabnBite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GrabnBite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

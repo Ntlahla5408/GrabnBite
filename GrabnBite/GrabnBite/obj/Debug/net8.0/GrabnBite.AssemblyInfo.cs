f2b3c83d8764194f4e6d0f647a41cd21387e1ab0
Ntlahla5408 <s226083470@Mandela.ac.za>
2026-09-15 17:02:39 +0200

    init

--- GrabnBite/GrabnBite/obj/Debug/net8.0/GrabnBite.AssemblyInfo.cs
+++ GrabnBite/GrabnBite/obj/Debug/net8.0/GrabnBite.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GrabnBite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2dfea16bae63d70512f821fa96ceec3e2159f816")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d5ea921954b35392898778344627e9ddee4fa57")]
 [assembly: System.Reflection.AssemblyProductAttribute("GrabnBite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GrabnBite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
